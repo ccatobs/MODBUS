ddc1300976919d74fc26aa4f5e7f0a653b3e2562
Tamburasca <rtimmermann@astro.uni-bonn.de>
2021-10-20 10:01:32 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
 # MODBUS
-Development of a general MODBUS interface, where all parameter are to be 
-defined in a JSON file.
+Development of a general MODBUS interface, where the mappings of  
+coil, discrete input, input register, and holding register are entirely
+defined in a 
+[JSON](https://github.com/ccatp/MODBUS/blob/master/src/client_mapping.json). 
+In case of gaps (for input and holding registers) 
+between the defined registers, the appropriate number of bytes is calculated 
+and skipped accordingly.
+
+The JSON format is to be defined in the following formate, e.g.:
+```
+{
+  "10000": {
+    "parameter": "UnitOn",                      # parameter mandatory
+    "desc": "Unit On status: TRUE = Unit ON"    # desc optional
+  },
+  "10001": {
+    "parameter": "Unit_Alarm"
+  },
+  ...
+  "30001": {
+    "function": "decode_16bit_uint",    # function mandatory for input and holding
+    "parameter": "Operating State",
+    "map": {
+      "0": "Idling ‐ ready to start",   # if map then desc = element of map
+      "2": "Starting",
+      "3": "Running",
+      "5": "Stopping",
+      "6": "Error Lockout",
+      "7": "Error",
+      "8": "Helium Cool Down",
+      "9": "Power related Error",
+      "15": "Recovered from Error"
+    }
+  },
+  ...                    # possible gaps between keys are automatically skipped
+  "30031": {
+    "function": "decode_16bit_uint",
+    "parameter": "Panel Serial Number",
+    "desc": "This is supposed to be the Panel Serial Number"
+  },
+  ...
+  "30033": {
+    "function": "decode_16bit_uint",
+    "parameter": "Software Rev"
+  },
+  "30034/2": {
+    "function": "decode_bits",
+    "parameter": "TEST",
+    "map": {
+      "0b00000001": "test0",    # for decode bits mapping in format "0b 
+      "0b00000010": "test1",
+      "0b00000100": "test2",
+      "0b00001000": "test3",
+      "0b00010000": "test4",
+      "0b00100000": "test5",
+      "0b01000000": "test6",
+      "0b10000000": "test7"
+    }
+  }
+}
+```
+A check on the uniqueness of "parameter" is performed. Keys are to be in the 
+format: 3xxxx, 3xxxx/1 or 3xxxx/2 for the leading/trailing byte or 3xxxx/3xxxx 
+for 32/64 bit spanning registers. 
+
+Caveat:
+* decoder.decode_string(size=1) - Decodes a string from the buffer
+* decoder.bit_chunks() - classmethod
+
+not considered yet.
 
 This repository comprises MODBUS
-* server simulator (the python code is from https://hub.docker.com/r/oitc/modbus-server)
+* server simulator (the python code is extracted from 
+https://hub.docker.com/r/oitc/modbus-server)
 * client at its early state (more to come)
 
--- src/modbus_client.py
+++ src/modbus_client.py
@@ -3,16 +3,22 @@ from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 import time
 import json
+import re
+import sys
 import logging
-
-myformat = "%(asctime)s.%(msecs)03d :: %(levelname)s: %(filename)s - %(lineno)s - %(funcName)s()\t%(message)s"
-logging.basicConfig(format=myformat,
-                    level=logging.INFO,
-                    datefmt="%Y-%m-%d %H:%M:%S")
-logging.getLogger().setLevel(logging.INFO)
 
 
 class BinaryStringError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class MappingKeyError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class DuplicateParameterError(Exception):
     """Base class for other exceptions"""
     pass
 
@@ -31,28 +37,31 @@ class ObjectType(object):
         self.client = client
         self.entity = entity
         # select mapping for each entity and sort by key
-        self.register_maps = {key: value for key, value in
-                              sorted(mapping.items()) if key[0] == self.entity}
+        self.register_maps = {
+            key: value for key, value in
+            sorted(mapping.items()) if key[0] == self.entity
+        }
+
         els = [i for i in self.register_maps]
         if not els:
             self.boundaries = {}
         else:
-            last = els[-1]
-            # length of key for e.g.
-            # '3xxxx/3xxxx' or
-            # '3xxxx/2 or just
-            # '3xxxx
             mini = els[0].split("/")[0]
-            if len(last) != 11:
-                maxi = last.split("/")[0]
-            else:
-                maxi = last.split("/")[1]
+            maxi = els[-1].split("/")[0]
+            if len(els[-1].split("/")) == 2:
+                if els[-1].split("/")[1] not in ['1', '2']:
+                    maxi = els[-1].split("/")[1]
             self.boundaries = {
                 "min": mini,
                 "max": maxi,
                 "start": int(mini[1:]),
                 "width": int(maxi[1:]) - int(mini[1:]) + 1
             }
+            if self.entity in ['0', '1']:
+                if self.boundaries['start'] + self.boundaries['width'] >= 2000:
+                    logging.error("Error: number of registers superseed "
+                                  "limit of 2000. Consider a redesign!")
+                    sys.exit(1)
 
     @staticmethod
     def gap(low, high):
@@ -69,7 +78,6 @@ class ObjectType(object):
         if len(hb) == 2:
             if hb[1] == "2":
                 byt += 1
-        # high = hb[0]
         lb = low.split("/")
         if len(lb) == 2:
             if lb[1] == "1":
@@ -95,16 +103,17 @@ class ObjectType(object):
             return tmp[::-1].index('1')
         except IndexError:
             logging.error("Error: no binary string in mapping")
+            sys.exit(1)
         except BinaryStringError:
             logging.error("Error: wrong binary string in mapping")
+            sys.exit(1)
 
-    def formatter(self, decoder, decoded, register):
+    def formatter(self, decoder, register):
         """
         format the output dictionary and append by scanning through the mapping
         of the registers. If gaps in the mappings are detected they are skipped
         by the number of bytes.
         :param decoder: A deferred response handle from the register readings
-        :param decoded: dictionary
         :param register: dictionary
         :return: dict
         """
@@ -113,6 +122,7 @@ class ObjectType(object):
         maps = self.register_maps[register].get('map')
         desc = self.register_maps[register].get('desc')
         value = getattr(decoder, function)()
+        decoded = list()
         if function == 'decode_bits':
             for key, name in self.register_maps[register]['map'].items():
                 decoded.append(
@@ -134,95 +144,134 @@ class ObjectType(object):
 
         return decoded
 
-    def formatter_bit(self, decoder, decoded, register):
+    def formatter_bit(self, decoder, register):
         """
         indexes the result array of bits by the keys found in the mapping
         :param decoder: A deferred response handle from the register readings
-        :param decoded: dictionary
         :param register: dictionary
         :return: dict
         """
         parameter = self.register_maps[register]['parameter']
         desc = self.register_maps[register].get('desc')
-        decoded.append({
-            "parameter": parameter,
-            "value": decoder[int(register[1:]) - 1],
-            "description": desc
-        })
-        print(int(register[1:]) - 1)
 
-        return decoded
+        return [
+            {
+                "parameter": parameter,
+                "value": decoder[int(register[1:])],
+                "description": desc
+            }
+        ]
 
     def run(self):
         """
         instantiates the classes for the 4 register object types and invokes
         the run methods within an interval.
-        :return: dictionary of output
+        These two methods are not yet implemented !!!
+            decoder.decode_string(size=1) - Decodes a string from the buffer
+            decoder.bit_chunks() - classmethod
+        :return: dictionary
         """
         if not self.boundaries:
             return []
         result = None
-        if self.entity == '1':
-            # for simplicity we read in the max of 2000 bits
-            result = self.client.read_discrete_inputs(address=0,
-                                                      count=2000)
-        elif self.entity == '2':
+        if self.entity == '0':
+            # ToDo: for simplicity we read first 2000 bits
             result = self.client.read_coils(address=0,
-                                            count=2000)
+                                            count=2000
+                                            )
+        elif self.entity == '1':
+            # ToDo: for simplicity we read first 2000 bits
+            result = self.client.read_discrete_inputs(address=0,
+                                                      count=2000
+                                                      )
         elif self.entity == '3':
             result = self.client.read_input_registers(
                 address=self.boundaries['start'],
-                count=self.boundaries['width'])
+                count=self.boundaries['width']
+            )
         elif self.entity == '4':
             result = self.client.read_holding_registers(
                 address=self.boundaries['start'],
-                count=self.boundaries['width'])
+                count=self.boundaries['width']
+            )
         assert (not result.isError())
 
         decoded = list()
         if self.entity in ['3', '4']:
-            decoder = BinaryPayloadDecoder.fromRegisters(result.registers,
-                                                         byteorder=Endian.Big)
+            decoder = BinaryPayloadDecoder.fromRegisters(
+                registers=result.registers,
+                byteorder=Endian.Big
+            )
             # loop incl. penultimate and find gaps not to be read-out
-            for index, item in enumerate(list(self.register_maps.keys())[:-1]):
-                decoded = self.formatter(
-                    decoder,
-                    decoded,
-                    item)
-                # find if there are bytes to skip
+            for index, register in enumerate(
+                    list(self.register_maps.keys())[:-1]):
+                decoded = decoded + self.formatter(
+                    decoder=decoder,
+                    register=register
+                )
+                # if there are bytes to skip
                 skip = self.gap(
-                    low=item,
-                    high=list(self.register_maps.keys())[index + 1])
-                decoder.skip_bytes(skip)
-            # last entry in dict
-            decoded = self.formatter(
-                decoder,
-                decoded,
-                list(self.register_maps.keys())[-1])
-        elif self.entity in ['1', '2']:
+                    low=register,
+                    high=list(self.register_maps.keys())[index + 1]
+                )
+                decoder.skip_bytes(nbytes=skip)
+            # last entry in dictionary
+            decoded = decoded + self.formatter(
+                decoder=decoder,
+                register=list(self.register_maps.keys())[-1])
+        elif self.entity in ['0', '1']:
             decoder = result.bits
-            for item in self.register_maps.keys():
-                decoded = self.formatter_bit(
-                    decoder,
-                    decoded,
-                    item)
+            for register in self.register_maps.keys():
+                decoded = decoded + self.formatter_bit(
+                    decoder=decoder,
+                    register=register
+                )
 
         return decoded
 
 
 def main():
 
-    with open('client_config.json') as json_file:
-        client_config = json.load(json_file)
+    with open('client_config.json') as config_file:
+        client_config = json.load(config_file)
+
+    myformat = "%(asctime)s.%(msecs)03d :: %(levelname)s: %(filename)s - %(lineno)s - %(funcName)s()\t%(message)s"
+    logging.basicConfig(format=myformat,
+                        level=logging.INFO,
+                        datefmt="%Y-%m-%d %H:%M:%S")
+    if client_config['debug']:
+        logging.getLogger().setLevel(logging.DEBUG)
 
     with open('client_mapping.json') as json_file:
         mapping = json.load(json_file)
+
+    # checks on the client mapping
+    # test on keys in mapping
+    # keys are of following formate: '3xxxx/3xxxx', '3xxxx/2 or '3xxxx'
+    rev_dict = dict()
+    try:
+        for key, value in mapping.items():
+            if not re.match(
+                    r"^[0134][0-9]{4}(/([12]|[0134][0-9]{4}))?$",
+                    key):
+                raise MappingKeyError
+            rev_dict.setdefault(value["parameter"], set()).add(key)
+            parameter = [key for key, values in rev_dict.items()
+                         if len(values) > 1]
+            if parameter:
+                raise DuplicateParameterError
+    except MappingKeyError:
+        logging.error("Error: wrong key in mapping: {0}".format(key))
+        return 1
+    except DuplicateParameterError:
+        logging.error("Error: duplicate parameters: {0}".format(parameter))
+        return 1
 
     client = ModbusClient(host=client_config["server"]["listenerAddress"],
                           port=client_config["server"]["listenerPort"])
     client.connect()
 
-    register_class = ['1', '2', '3', '4']
+    register_class = ['0', '1', '3', '4']
     instance_list = list()
     for regs in register_class:
         instance_list.append(ObjectType(client=client,
