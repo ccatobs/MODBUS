2a02a37156fa1bc60b82fc8ee92aa56da90082d4
Tamburasca <rtimmermann@astro.uni-bonn.de>
2021-11-29 17:07:18 +0100

    MODBUS Rest API for dynamical calls

--- src/modbus_REST_API.py
+++ src/modbus_REST_API.py
@@ -1,23 +1,68 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request, Response, g
 import modbus_writer
 import modbus_client
 import logging
 import os
 import json
+from threading import Lock
 from timeit import default_timer as timer
 
 app = Flask(__name__)
 
+# ToDo
+config = {"lhx": {
+    "mapping": "file1",
+    "config": "file2"
+}}
 
-@app.route('/write', methods=['PUT'])
-def write():
+
+class LockGroup(object):
+    """
+    source:
+    https://stackoverflow.com/questions/37624289/value-based-thread-lock
+    """
+    def __init__(self):
+        self.lock_dict = {}
+        self.__lock = Lock()
+
+    # Returns a lock object, unique for each unique value of param.
+    # The first call with a given value of param creates a new lock, subsequent
+    # calls return the same lock.
+    def __call__(self, param: str = None):
+        with self.__lock:
+            if param not in self.lock_dict:
+                self.lock_dict[param] = Lock()
+            return self.lock_dict[param]
+
+
+lock_mb_client = LockGroup()
+
+
+@app.teardown_request
+def teardown_request(error=None):
+    if error:
+        logging.error(
+            "Teardown_request: cleaning up...: {0}".format(str(error))
+        )
+    if lock_mb_client(g.name).locked():
+        logging.warning(
+            "Release lock for: {0}".format(g.name)
+        )
+        lock_mb_client(g.name).release()
+
+
+@app.route('/<name>/write', methods=['PUT'])
+def write(name: str = None) -> json:
     _start_time = timer()
+    g.name = name
 
+    lock_mb_client(name).acquire()
     payload = request.json
     initial = modbus_writer.initialize()
     modbus_writer.writer(init=initial,
                          wr=payload)
     modbus_writer.close(client=initial["client"])
+    lock_mb_client(name).release()
 
     logging.info("Time consumed to process modbus writer: {0:.1f} ms".format(
         (timer() - _start_time) * 1000)
@@ -27,13 +72,16 @@ def write():
                     status=201)
 
 
-@app.route('/read', methods=['GET'])
-def read():
+@app.route('/<name>/read', methods=['GET'])
+def read(name: str = None) -> json:
     _start_time = timer()
-
+    g.name = name
+    
+    lock_mb_client(name).acquire()
     initial = modbus_client.initialize()
     result = modbus_client.retrieve(init=initial)
     modbus_client.close(client=initial["client"])
+    lock_mb_client(name).release()
 
     logging.info("Time consumed to process modbus reader: {0:.1f} ms".format(
         (timer() - _start_time) * 1000)
@@ -57,3 +105,4 @@ if __name__ == '__main__':
                 threaded=False)
     except:
         logging.error("Unable to open port")
+        exit(1)
