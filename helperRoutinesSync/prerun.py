8798f401a35b02b6e1b6d6e7c10525c3c8506cd4
Tamburasca <rtimmermann@astro.uni-bonn.de>
2023-08-25 21:23:50 +0200

    v5.0.5 set PYTHONPATH when executing helper routines

--- helperRoutinesSync/prerun.py
+++ helperRoutinesSync/prerun.py
@@ -5,8 +5,6 @@ import sys
 class PreRun(object):
     if os.environ.get('PYTHONPATH') is None:
         sys.path.append("{0}{1}".format(
-            os.path.dirname(
-                os.path.realpath(__file__)
-            ),
+            os.path.dirname(os.path.realpath(__file__)),
             "/../")
         )
